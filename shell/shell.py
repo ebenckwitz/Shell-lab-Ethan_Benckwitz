0bcb5e950077029efb536a1cfa8b812dd3ff1359
Ethan <ebenckwitz>
2020-09-07 20:23:22 -0600

    more progress

--- shell/shell.py
+++ shell/shell.py
@@ -2,15 +2,16 @@
 #Author: Ethan Benckwitz
 import os, sys, re
 
-command = input("$ ")
-while command != "exit":
-    my_args = command.split()
-    my_shell(my_args[0], my_args[1])
-    command = input("S ")
-print("It is over.")
+def beginning():
+    command = input("$ ")
+    while command != "exit":
+        my_args = command.split()
+        my_shell(my_args[0], my_args[1])
+        command = input("$ ")
+    print("It is over.")
 
-def my_shell(arg1, arg2):
-    pid = os.getipid()
+def my_shell(arg0, arg1):
+    pid = os.getpid()
     os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
     
     rc = os.fork()
@@ -36,4 +37,6 @@ def my_shell(arg1, arg2):
     else:
         os.write(1, ("This is a parent! Parent's pid=%d Child's pid=%d\n" % (pid, rc)).encode())
         waiting = os.wait()
-        os.write(1, ("Parent: Child %d terminated with exit code %d\n" % waiting).enocde())
+        os.write(1, ("Parent: Child %d terminated with exit code %d\n" % waiting).encode())
+        
+beginning()
