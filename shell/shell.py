b831a510b462353f595a721aa9e32997b2453ea1
Ethan <ebenckwitz>
2020-09-12 14:46:29 -0600

    adding redirecting

--- shell/shell.py
+++ shell/shell.py
@@ -5,16 +5,17 @@ import os, sys, re
 def beginning():
     command = input("$ ")
     while command != "exit":
-        my_args = command.split()
-        my_shell(my_args[0], my_args[1])
+        #my_args = command.split()
+        my_shell(command)
         command = input("$ ")
-    print("It is over.")
+    sys.exit(1)
 
-def my_shell(arg0, arg1):
+def my_shell(command):
     pid = os.getpid()
     os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
     
     rc = os.fork()
+    args = command.split()
 
     if rc < 0:
         os.write(2, ("Fork failed, returning %d\n" % rc).encode())
@@ -22,7 +23,10 @@ def my_shell(arg0, arg1):
 
     elif rc == 0:      #child
         os.write(1, ("This is a child! Child's pid=%d Parent's pid=%d\n" % (os.getpid(),pid)).encode())
-        args = [arg0, arg1]
+        os.close(1)
+        os.open("testing.txt", os.O_CREAT | os.O_WRONLY);
+        os.set_inheritable(1, True)
+        
         for dir in re.split(":", os.environ['PATH']): #try each directory in the path
             program = "%s/%s" % (dir, args[0])
             os.write(1, ("Child is trying to exec %s\n" % program).encode())
@@ -30,7 +34,7 @@ def my_shell(arg0, arg1):
                 os.execve(program, args, os.environ) #try to exec program
             except FileNotFoundError:                #this is expected
                 pass                                 #fail quietly
-            break
+            
         os.write(2, ("Child was not able to exec %s\n" % args[0]).encode())
         sys.exit(1)                                  #terminate with error
 
